8da13b6e78d367bb96e05fbe8f3f06183fdb3416
Javier Sánchez Mendoza <javier.sanchezm@sat.gob.mx>
2024-04-10 00:33:06 -0600

    // FEAT: center

--- app/src/main/java/com/javier1nc/businesscard/MainActivity.kt
+++ app/src/main/java/com/javier1nc/businesscard/MainActivity.kt
@@ -3,14 +3,30 @@ package com.javier1nc.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.javier1nc.businesscard.ui.theme.BusinessCardTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,10 +47,34 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BusinessCard(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .background(Color(0xFFd1e4cf)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(modifier = Modifier.background(Color.Black)){
+            val image = painterResource(R.drawable.android_logo)
+            Image(
+                painter = image, contentDescription = null, Modifier.fillMaxWidth(0.3f)
+            )
+        }
+
+        Text(
+            text = "Javier1nc",
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
+            fontSize = 50.sp,
+            fontWeight = FontWeight.Light,
+        )
+        Text(
+            text = "Android Developer Extraordinary",
+            modifier = modifier,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF3a5332)
+        )
+    }
 }
 
 @Preview(showBackground = true)
