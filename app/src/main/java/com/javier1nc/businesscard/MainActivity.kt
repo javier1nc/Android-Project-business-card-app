9058741db80d9a6df134f5dd954ab9a4ebd2c593
Javier Sánchez Mendoza <javier.sanchezm@sat.gob.mx>
2024-04-09 23:41:00 -0600

    // FEAT: initial structure

--- app/src/main/java/com/javier1nc/businesscard/MainActivity.kt
+++ app/src/main/java/com/javier1nc/businesscard/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    BusinessCard("Android")
                 }
             }
         }
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun BusinessCard(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
@@ -39,8 +39,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BusinessCardPreview() {
     BusinessCardTheme {
-        Greeting("Android")
+        BusinessCard("Android")
     }
 }
